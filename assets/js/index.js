47c248c501f14ab1f23f54c1067ef062c5655250
MysterY <mysteryofficial77@gmail.com>
2022-03-15 21:36:09 +0100

    v0.1
    Added homepage of my website

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+function darkmode() {
+    var mc = document.querySelector(".minecraft"), ap = document.querySelector(".aveeplayer");
+    mc.classList.toggle("dark-mode");
+    ap.classList.toggle("dark-mode");
+}
